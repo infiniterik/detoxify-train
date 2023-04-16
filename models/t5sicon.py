461cfbdc78d0d21bde69ed8cb4c8d71d14e4c9ea
Rik Bose <rbose@ihmc.org>
2023-04-16 10:02:36 -0500

    coma

--- models/t5sicon.py
+++ models/t5sicon.py
@@ -21,7 +21,7 @@ def train(train_df, eval_df, prototype="t5", base_model="t5-large", logger="defa
                 early_stopping_patience_epochs = args.get("early_stopping_patience_epochs", 3),
                 precision = args.get("precision", 16),
                 dataloader_num_workers= args.get("dataloader_num_workers", 0),
-                save_only_last_epoch= args.get("save_only_last_epoch", True)
+                save_only_last_epoch= args.get("save_only_last_epoch", True),
                 logger=logger
     )
 
