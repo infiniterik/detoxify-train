b44455da3ad3f7e860e993af2ccbe016d571ee65
Rik Bose <rbose@ihmc.org>
2023-04-17 09:52:49 -0500

    Smaller batch size?

--- models/t5sicon.py
+++ models/t5sicon.py
@@ -14,15 +14,16 @@ def train(train_df, eval_df, prototype="t5", base_model="t5-large", logger="defa
                 eval_df=eval_df, # pandas dataframe with 2 columns: source_text & target_text
                 source_max_token_len = args.get("source_max_token_len", 512), 
                 target_max_token_len = args.get("target_max_token_len", 512),
-                batch_size = args.get("batch_size", 8),
+                batch_size = args.get("batch_size", 2),
                 max_epochs = args.get("max_epochs", 3),
                 use_gpu = args.get("use_gpu", 3),
                 outputdir = args.get("output_dir", "outputs"),
                 early_stopping_patience_epochs = args.get("early_stopping_patience_epochs", 3),
                 precision = args.get("precision", 32),
-                dataloader_num_workers= args.get("dataloader_num_workers", 0),
+                dataloader_num_workers= args.get("dataloader_num_workers", 32),
                 save_only_last_epoch= args.get("save_only_last_epoch", True),
-                logger=logger
+                logger=logger,
+                strategy="ddp"
     )
 
     model.model.save_pretrained(args.get("output_dir", "outputs")+"_model")
