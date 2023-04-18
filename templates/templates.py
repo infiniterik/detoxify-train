e4c9dd2e963acf9a799aae46d6246cd2682f5ff2
Rik Bose <rbose@ihmc.org>
2023-04-18 18:23:28 -0500

    fixed path.

--- templates/templates.py
+++ templates/templates.py
@@ -150,7 +150,7 @@ def get_parent_child_toxic_summary(df, parent_column="text_x",
 def process_data(config):
     res = {}
     for split in ["train", "eval", "test"]:
-        load_data(f'artifacts/{config["base-dataset"]}:{config['base-version']}/')
+        load_data(f'artifacts/{config["base-dataset"]}:{config["base-version"]}/')
         fn = config.get("preprocess", "get_parent_child")
         args = {k: v for k, v in config.get("args", {}).items()}
         args["df"] = data
