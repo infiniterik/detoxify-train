679ebb9702caa3b5929a7cfb7bb4f4d840460e2d
Rik Bose <rbose@ihmc.org>
2023-04-20 03:09:54 -0500

    added full path

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,6 @@
 for f in `ls configs/t5run`; 
-    do python -c "import runt5; runt5.train_t5('$f')"; 
-    python -c "import runt5; runt5.test_t5('$f')";
+    do python -c "import runt5; runt5.train_t5('configs/t5run/$f')"; 
+    python -c "import runt5; runt5.test_t5('configs/t5run/$f')";
     rm -rf outputs; 
     rm -rf outputs_model;
 done
